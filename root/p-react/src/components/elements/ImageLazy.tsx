c6f36d4ee0b06426eef053061302015cfdaed6dc
Simon <sem.schakschin2010@proton.me>
2024-07-01 23:25:58 +0400

    use logos

--- root/p-react/src/components/elements/ImageLazy.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ImageProps } from '@/types/ImageProps';
-
-const LazyImage: React.FC<ImageProps> = ({ image, alt }) => {
-	return <img src={image} alt={alt} className="app-image" />;
-};
-
-export default LazyImage;
